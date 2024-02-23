ec94a2643801547d103e02ff900361cc63cead3f
Haziel Ortiz <ya-yo22124@outlook.com>
2024-02-23 15:25:56 -0600

    Fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import * as dotenv from "dotenv";
-
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 
-dotenv.config();
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.SERVER_PORT!);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Configurar CORS
+  app.enableCors({
+    origin: "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Habilita el intercambio de cookies (si es necesario)
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
